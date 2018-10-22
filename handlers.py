6a9ed453dbb424165efd142873fd722247ae3ce5
Dai Vu <dai.vu@edge-works.net>
2018-10-22 13:31:36 +0700

    fix(handler): Fix handle number column of file

--- handlers.py
+++ handlers.py
@@ -1062,7 +1062,7 @@ def mercari_data_handler(source_file_id, fname, conn=None, dest_table='Environme
     integrate_params = []
     for item in sql_params:
         if item[0] != item[1] and item[0] != item[2] and item[0] != item[3]:
-            if len(item) > 11:
+            if len(item) > 12:
               del item[len(item) - 2]
             if item[0] is None:
                 item[0] = 'ESC'
